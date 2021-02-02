a6c5e7e43eac2a5c714c4bd6becd69c61e803ac1
joseluisgs <joseluisgonsan@gmail.com>
2021-02-02 13:52:37 +0100

    Navbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+// NAVBAR
+// Seleccionamos los id y cambiamos los estilos
+const burgerIcon = document.querySelector('#navbar-menu-burguer');
+const navbarMenu = document.querySelector('#navbar-menu-items');
+
+// Cuando se pulsa cambiamos la propiedad is-active
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
+
