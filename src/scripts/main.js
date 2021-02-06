5959f63bbf0e38412cba3cc44799cd6c0758d002
joseluisgs <joseluisgonsan@gmail.com>
2021-02-06 12:46:38 +0100

    Modo socuro Vanila JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,5 +19,39 @@ const navbarMenu = document.querySelector('#navbar-menu-items');
 // Cuando se pulsa cambiamos la propiedad is-active
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
+    burgerIcon.classList.toggle('is-active');
 });
+
+// CAMBIO DE TEMA
+const toggleSwitch = document.querySelector('#temaCheckbox');
+console.log(toggleSwitch);
+const currentTheme = localStorage.getItem('theme');
+console.log(currentTheme);
+
+if (currentTheme) {
+    // añadimos o cambiamos la etiqueta de la clase html al tema recuperado
+    let htmlElement = document.documentElement;
+    htmlElement.setAttribute('theme', currentTheme);
+
+    // Si es el tema oscuro lo marcamos
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    // Si esta marcado es el tema oscuro
+    let htmlElement = document.documentElement;
+    if (e.target.checked) {
+        htmlElement.setAttribute('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    // Si no es el tema claro
+    else {
+        htmlElement.setAttribute('theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
